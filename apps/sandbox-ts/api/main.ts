116523db751dc5a0a89fd71e668d87af34552bf8
Anand Raj SinghDAD0-D779 <singhpariharanandraj@gmail.com>
2026-01-22 20:03:57 +0530

    initialized http server

--- apps/sandbox-ts/api/main.ts
+++ apps/sandbox-ts/api/main.ts
@@ -1,1 +1,12 @@
-console.log("hello")
+import http from "http";
+
+const server = http.createServer((req,res) => {
+    if(req.method === "POST" && req.url === "/exec"){
+        res.end(JSON.stringify({res: "exec url"}))
+    }
+    return
+})
+
+server.listen(3000, () => {
+    console.log("API listening on port 3000")
+})
